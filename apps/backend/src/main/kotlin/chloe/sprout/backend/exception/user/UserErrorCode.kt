8a68d257ffeb47cd42f0e80fee4b11475702927a
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-07-17 23:48:40 +0900

    feat: add ErrorCode and Exceptions related Jwt for error handling
    - UserErrorCode에 Jwt 관련 enum 추가
    - UserException에 Jwt 관련 exception class 추가

--- apps/backend/src/main/kotlin/chloe/sprout/backend/exception/user/UserErrorCode.kt
+++ apps/backend/src/main/kotlin/chloe/sprout/backend/exception/user/UserErrorCode.kt
@@ -13,7 +13,11 @@ enum class UserErrorCode(
     ACCESS_DENIED(HttpStatus.UNAUTHORIZED.value(), "USER_002", "인증되지 않은 사용자입니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "USER_003", "사용자를 찾을 수 없습니다."),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED.value(), "USER_004", "비밀번호가 일치하지 않습니다."),
-    USER_ALREADY_EXISTS(HttpStatus.CONFLICT.value(), "USER_005", "이미 존재하는 이메일입니다.");
+    USER_ALREADY_EXISTS(HttpStatus.CONFLICT.value(), "USER_005", "이미 존재하는 이메일입니다."),
+    INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED.value(), "USER_006", "유효하지 않은 Access Token입니다."),
+    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED.value(), "USER_007", "유효하지 않은 Refresh Token입니다."),
+    MISSING_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED.value(), "USER_008", "요청에 Refresh Token을 찾을 수 없습니다."),
+    LOGIN_REQUIRED(HttpStatus.UNAUTHORIZED.value(), "USER_009", "계정에 로그인해야 합니다.");
 
     override fun getErrorDetail(): ErrorDetail {
         return ErrorDetail(status, code, message)
--- apps/backend/src/main/kotlin/chloe/sprout/backend/exception/user/UserException.kt
+++ apps/backend/src/main/kotlin/chloe/sprout/backend/exception/user/UserException.kt
@@ -2,6 +2,12 @@ package chloe.sprout.backend.exception.user
 
 import chloe.sprout.backend.exception.CustomException
 
+class ForbiddenException() : CustomException(UserErrorCode.FORBIDDEN)
+class AccessDeniedException() : CustomException(UserErrorCode.ACCESS_DENIED)
 class UserNotFoundException() : CustomException(UserErrorCode.USER_NOT_FOUND)
 class InvalidPasswordException() : CustomException(UserErrorCode.INVALID_PASSWORD)
 class UserAlreadyExistsException() : CustomException(UserErrorCode.USER_ALREADY_EXISTS)
+class InvalidAccessTokenException() : CustomException(UserErrorCode.INVALID_ACCESS_TOKEN)
+class InvalidRefreshTokenException() : CustomException(UserErrorCode.INVALID_REFRESH_TOKEN)
+class MissingRefreshTokenException() : CustomException(UserErrorCode.MISSING_REFRESH_TOKEN)
+class LoginRequiredException() : CustomException(UserErrorCode.LOGIN_REQUIRED)

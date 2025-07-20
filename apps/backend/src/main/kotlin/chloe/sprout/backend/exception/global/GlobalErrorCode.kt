fc65cc603c930bd42b5564a5969ec630d378e05d
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-07-20 23:49:52 +0900

    docs: update messages in GlobalErrorCode
    - 전체 메시지의 일관성 유지를 위해 변경

--- apps/backend/src/main/kotlin/chloe/sprout/backend/exception/global/GlobalErrorCode.kt
+++ apps/backend/src/main/kotlin/chloe/sprout/backend/exception/global/GlobalErrorCode.kt
@@ -9,7 +9,7 @@ enum class GlobalErrorCode(
     val code: String,
     val message: String
 ) : BaseErrorCode {
-    INVALID_FIELD_VALUE(HttpStatus.BAD_REQUEST.value(), "GLOBAL_001", "잘못된 필드 값입니다.");
+    INVALID_FIELD_VALUE(HttpStatus.BAD_REQUEST.value(), "GLOBAL_001", "유효하지 않은 필드 값입니다.");
 
     override fun getErrorDetail(): ErrorDetail {
         return ErrorDetail(status, code, message)

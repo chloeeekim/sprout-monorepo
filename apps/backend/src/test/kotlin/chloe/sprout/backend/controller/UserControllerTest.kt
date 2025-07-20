0cf14d6ab505b89207ee0be77c1d459778624710
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-07-21 00:52:49 +0900

    test: add failure case tests for UserController
    - 로그인 API 관련 실패 케이스 추가
    - 토큰 재발급 및 로그아웃 API에 대해서는 AuthenticationException이 발생하기 때문에 Controller 테스트에서는 제외 (통합 테스트에 포함 예정)

--- apps/backend/src/test/kotlin/chloe/sprout/backend/controller/UserControllerTest.kt
+++ apps/backend/src/test/kotlin/chloe/sprout/backend/controller/UserControllerTest.kt
@@ -1,13 +1,15 @@
 package chloe.sprout.backend.controller
 
 import chloe.sprout.backend.annotation.CustomWebMvcTest
-import chloe.sprout.backend.auth.JwtAuthenticationFilter
-import chloe.sprout.backend.config.SecurityConfig
 import chloe.sprout.backend.dto.UserLoginRequest
 import chloe.sprout.backend.dto.UserLoginResponse
 import chloe.sprout.backend.dto.UserSignupRequest
 import chloe.sprout.backend.dto.UserSignupResponse
+import chloe.sprout.backend.exception.global.GlobalErrorCode
+import chloe.sprout.backend.exception.user.InvalidPasswordException
 import chloe.sprout.backend.exception.user.UserAlreadyExistsException
+import chloe.sprout.backend.exception.user.UserErrorCode
+import chloe.sprout.backend.exception.user.UserNotFoundException
 import chloe.sprout.backend.service.UserService
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.ninjasquad.springmockk.MockkBean
@@ -17,13 +19,7 @@ import jakarta.servlet.http.Cookie
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.FilterType
 import org.springframework.http.MediaType
-import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.post
 import java.util.*
@@ -65,16 +61,19 @@ class UserControllerTest{
 
     @Test
     @DisplayName("회원가입 API - 실패 (잘못된 요청 데이터)")
-    fun signupApi_failure_invalidRequest() {
+    fun signupApi_fail_invalidRequest() {
         // given
         val invalidRequest = UserSignupRequest("not-an-email", "password", "")
+        val errorDetail = GlobalErrorCode.INVALID_FIELD_VALUE.getErrorDetail()
 
         // when & then
         mockMvc.post("/api/users/signup") {
             contentType = MediaType.APPLICATION_JSON
             content = objectMapper.writeValueAsString(invalidRequest)
         }.andExpect {
-            status { isBadRequest() }
+            status { isEqualTo(errorDetail.status) }
+            jsonPath("$.code") { value(errorDetail.code)}
+            jsonPath("$.message") { value(errorDetail.message)}
         }
 
         verify(exactly = 0) { userService.signup(any()) }
@@ -82,9 +81,10 @@ class UserControllerTest{
 
     @Test
     @DisplayName("회원가입 API - 실패 (이미 존재하는 이메일")
-    fun signupApi_failure_emailAlreadyExists() {
+    fun signupApi_fail_emailAlreadyExists() {
         // given
         val request = UserSignupRequest("test@test.com", "password", "name")
+        val errorDetail = UserErrorCode.USER_ALREADY_EXISTS.getErrorDetail()
 
         every { userService.signup(any()) } throws UserAlreadyExistsException()
 
@@ -92,7 +92,9 @@ class UserControllerTest{
             contentType = MediaType.APPLICATION_JSON
             content = objectMapper.writeValueAsString(request)
         }.andExpect {
-            status { isConflict() }
+            status { isEqualTo(errorDetail.status) }
+            jsonPath("$.code") { value(errorDetail.code) }
+            jsonPath("$.message") { value(errorDetail.message) }
         }
 
         verify(exactly = 1) { userService.signup(any()) }
@@ -116,6 +118,50 @@ class UserControllerTest{
             jsonPath("$.data.id") { exists() }
             jsonPath("$.data.email") { value(request.email) }
             jsonPath("$.data.name") { value("name")}
+        }
+
+        verify(exactly = 1) { userService.login(any(), any(), any()) }
+    }
+
+    @Test
+    @DisplayName("로그인 API - 실패 (사용자 없음)")
+    fun loginApi_fail_userNotFound() {
+        // given
+        val request = UserLoginRequest("test@test.com", "password")
+        val errorDetail = UserErrorCode.USER_NOT_FOUND.getErrorDetail()
+
+        every { userService.login(any(), any(), any()) } throws UserNotFoundException()
+
+        // when & then
+        mockMvc.post("/api/users/login") {
+            contentType = MediaType.APPLICATION_JSON
+            content = objectMapper.writeValueAsString(request)
+        }.andExpect {
+            status { isEqualTo(errorDetail.status) }
+            jsonPath("$.code") { value(errorDetail.code) }
+            jsonPath("$.message") { value(errorDetail.message) }
+        }
+
+        verify(exactly = 1) { userService.login(any(), any(), any()) }
+    }
+
+    @Test
+    @DisplayName("로그인 API - 실패 (비밀번호 불일치)")
+    fun loginApi_fail_invalidPassword() {
+        // given
+        val request = UserLoginRequest("test@test.com", "password")
+        val errorDetail = UserErrorCode.INVALID_PASSWORD.getErrorDetail()
+
+        every { userService.login(any(), any(), any()) } throws InvalidPasswordException()
+
+        // when & then
+        mockMvc.post("/api/users/login") {
+            contentType = MediaType.APPLICATION_JSON
+            content = objectMapper.writeValueAsString(request)
+        }.andExpect {
+            status { isEqualTo(errorDetail.status) }
+            jsonPath("$.code") { value(errorDetail.code) }
+            jsonPath("$.message") { value(errorDetail.message) }
         }
 
         verify(exactly = 1) { userService.login(any(), any(), any()) }

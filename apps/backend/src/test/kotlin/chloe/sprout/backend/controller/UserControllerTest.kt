356f8ca78c48c11e3b259f6fe092e515921733d9
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-07-22 21:23:55 +0900

    test: refactor UserControllerTest for consistency
    - DisplayName의 일관성 유지를 위해 문구 변경
    - 유지보수성을 높이기 위한 정리 작업 포함

--- apps/backend/src/test/kotlin/chloe/sprout/backend/controller/UserControllerTest.kt
+++ apps/backend/src/test/kotlin/chloe/sprout/backend/controller/UserControllerTest.kt
@@ -1,6 +1,7 @@
 package chloe.sprout.backend.controller
 
 import chloe.sprout.backend.annotation.CustomWebMvcTest
+import chloe.sprout.backend.domain.User
 import chloe.sprout.backend.dto.UserLoginRequest
 import chloe.sprout.backend.dto.UserLoginResponse
 import chloe.sprout.backend.dto.UserSignupRequest
@@ -11,11 +12,13 @@ import chloe.sprout.backend.exception.user.UserAlreadyExistsException
 import chloe.sprout.backend.exception.user.UserErrorCode
 import chloe.sprout.backend.exception.user.UserNotFoundException
 import chloe.sprout.backend.service.UserService
+import chloe.sprout.backend.util.TestUtils
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.ninjasquad.springmockk.MockkBean
 import io.mockk.every
 import io.mockk.verify
 import jakarta.servlet.http.Cookie
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -36,12 +39,24 @@ class UserControllerTest{
     @MockkBean
     private lateinit var userService: UserService
 
+    private lateinit var testUser: User
+
+    @BeforeEach
+    fun setUp() {
+        testUser = User(
+            email = "test@test.com",
+            name = "name",
+            password = "password"
+        )
+        TestUtils.setSuperClassPrivateField(testUser, "id", UUID.fromString("999999999999-9999-9999-999999999999"))
+    }
+
     @Test
-    @DisplayName("회원가입 API - 성공")
+    @DisplayName("POST /api/users/signup(회원가입) - 성공")
     fun signupApi_success() {
         // given
-        val request = UserSignupRequest("test@test.com", "password", "name")
-        val response = UserSignupResponse(UUID.randomUUID(), request.email, request.name)
+        val request = UserSignupRequest(testUser.email, testUser.password, testUser.name)
+        val response = UserSignupResponse(testUser.id, testUser.email, testUser.name)
 
         every { userService.signup(any()) } returns response
 
@@ -51,19 +66,19 @@ class UserControllerTest{
             content = objectMapper.writeValueAsString(request)
         }.andExpect {
             status { isCreated() }
-            jsonPath("$.data.id") { exists() }
-            jsonPath("$.data.email") { value(request.email) }
-            jsonPath("$.data.name") { value(request.name) }
+            jsonPath("$.data.id") { value(testUser.id.toString()) }
+            jsonPath("$.data.email") { value(testUser.email) }
+            jsonPath("$.data.name") { value(testUser.name) }
         }
 
-        verify(exactly = 1) { userService.signup(any()) }
+        verify(exactly = 1) { userService.signup(request) }
     }
 
     @Test
-    @DisplayName("회원가입 API - 실패 (잘못된 요청 데이터)")
+    @DisplayName("POST /api/users/signup(회원가입) - 실패 (잘못된 요청 데이터)")
     fun signupApi_fail_invalidRequest() {
         // given
-        val invalidRequest = UserSignupRequest("not-an-email", "password", "")
+        val invalidRequest = UserSignupRequest("not-an-email", testUser.password, "")
         val errorDetail = GlobalErrorCode.INVALID_FIELD_VALUE.getErrorDetail()
 
         // when & then
@@ -76,14 +91,14 @@ class UserControllerTest{
             jsonPath("$.message") { value(errorDetail.message)}
         }
 
-        verify(exactly = 0) { userService.signup(any()) }
+        verify(exactly = 0) { userService.signup(invalidRequest) }
     }
 
     @Test
-    @DisplayName("회원가입 API - 실패 (이미 존재하는 이메일")
+    @DisplayName("POST /api/users/signup(회원가입) - 실패 (이미 존재하는 이메일")
     fun signupApi_fail_emailAlreadyExists() {
         // given
-        val request = UserSignupRequest("test@test.com", "password", "name")
+        val request = UserSignupRequest(testUser.email, testUser.password, testUser.name)
         val errorDetail = UserErrorCode.USER_ALREADY_EXISTS.getErrorDetail()
 
         every { userService.signup(any()) } throws UserAlreadyExistsException()
@@ -97,15 +112,15 @@ class UserControllerTest{
             jsonPath("$.message") { value(errorDetail.message) }
         }
 
-        verify(exactly = 1) { userService.signup(any()) }
+        verify(exactly = 1) { userService.signup(request) }
     }
 
     @Test
-    @DisplayName("로그인 API - 성공")
+    @DisplayName("POST /api/users/login (로그인) - 성공")
     fun loginApi_success() {
         // given
-        val request = UserLoginRequest("test@test.com", "password")
-        val response = UserLoginResponse(UUID.randomUUID(), request.email, "name")
+        val request = UserLoginRequest(testUser.email, testUser.password)
+        val response = UserLoginResponse(testUser.id, testUser.email, testUser.name)
 
         every { userService.login(any(), any(), any()) } returns response
 
@@ -115,19 +130,19 @@ class UserControllerTest{
             content = objectMapper.writeValueAsString(request)
         }.andExpect {
             status { isOk() }
-            jsonPath("$.data.id") { exists() }
-            jsonPath("$.data.email") { value(request.email) }
-            jsonPath("$.data.name") { value("name")}
+            jsonPath("$.data.id") { value(testUser.id.toString()) }
+            jsonPath("$.data.email") { value(testUser.email) }
+            jsonPath("$.data.name") { value(testUser.name) }
         }
 
-        verify(exactly = 1) { userService.login(any(), any(), any()) }
+        verify(exactly = 1) { userService.login(request, any(), any()) }
     }
 
     @Test
-    @DisplayName("로그인 API - 실패 (사용자 없음)")
+    @DisplayName("POST /api/users/login (로그인) - 실패 (사용자 없음)")
     fun loginApi_fail_userNotFound() {
         // given
-        val request = UserLoginRequest("test@test.com", "password")
+        val request = UserLoginRequest(testUser.email, testUser.password)
         val errorDetail = UserErrorCode.USER_NOT_FOUND.getErrorDetail()
 
         every { userService.login(any(), any(), any()) } throws UserNotFoundException()
@@ -142,14 +157,14 @@ class UserControllerTest{
             jsonPath("$.message") { value(errorDetail.message) }
         }
 
-        verify(exactly = 1) { userService.login(any(), any(), any()) }
+        verify(exactly = 1) { userService.login(request, any(), any()) }
     }
 
     @Test
-    @DisplayName("로그인 API - 실패 (비밀번호 불일치)")
+    @DisplayName("POST /api/users/login (로그인) - 실패 (비밀번호 불일치)")
     fun loginApi_fail_invalidPassword() {
         // given
-        val request = UserLoginRequest("test@test.com", "password")
+        val request = UserLoginRequest(testUser.email, testUser.password)
         val errorDetail = UserErrorCode.INVALID_PASSWORD.getErrorDetail()
 
         every { userService.login(any(), any(), any()) } throws InvalidPasswordException()
@@ -164,11 +179,11 @@ class UserControllerTest{
             jsonPath("$.message") { value(errorDetail.message) }
         }
 
-        verify(exactly = 1) { userService.login(any(), any(), any()) }
+        verify(exactly = 1) { userService.login(request, any(), any()) }
     }
 
     @Test
-    @DisplayName("토큰 재발급 API - 성공")
+    @DisplayName("POST /api/users/refresh (토큰 재발급) - 성공")
     fun refreshApi_success() {
         // given
         val refreshToken = "refreshToken"
@@ -185,7 +200,7 @@ class UserControllerTest{
     }
 
     @Test
-    @DisplayName("로그아웃 API - 성공")
+    @DisplayName("POST /api/users/logout (로그아웃) - 성공")
     fun logoutApi_success() {
         // given
         val accessToken = "accessToken"

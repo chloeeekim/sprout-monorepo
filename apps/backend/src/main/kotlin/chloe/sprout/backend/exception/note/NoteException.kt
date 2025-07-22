603fe9b69c2a0b4c7a9b9daafec03f58c008f99e
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-07-22 21:41:35 +0900

    fix: correct typo in NoteErrorCode from TITLE_TITLE_REQUIRED to NOTE_TITLE_REQUIRED
    - NoteErrorCode에 잘못 기재된 TITLE_TITLE_REQUIRED를 NOTE_TITLE_REQUIRED로 수정

--- apps/backend/src/main/kotlin/chloe/sprout/backend/exception/note/NoteException.kt
+++ apps/backend/src/main/kotlin/chloe/sprout/backend/exception/note/NoteException.kt
@@ -3,5 +3,5 @@ package chloe.sprout.backend.exception.note
 import chloe.sprout.backend.exception.CustomException
 
 class NoteNotFoundException() : CustomException(NoteErrorCode.NOTE_NOT_FOUND)
-class NoteTitleRequiredException() : CustomException(NoteErrorCode.TITLE_TITLE_REQUIRED)
+class NoteTitleRequiredException() : CustomException(NoteErrorCode.NOTE_TITLE_REQUIRED)
 class NoteOwnerMismatchException() : CustomException(NoteErrorCode.NOTE_OWNER_MISMATCH)

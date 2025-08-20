afe806969a23dc8f65916b77b7fd613b9ca38017
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-08-21 00:39:59 +0900

    refactor: merge Note pages into a single NotePage with redesigned UI
    - NoteCreatePage, NoteEditPage, NoteDetailPage를 하나의 NotePage로 통합
    - 레이아웃 및 스타일 변경

---
+++ apps/frontend/src/pages/note/NotePage.tsx
@@ -0,0 +1,206 @@
+import React, { useEffect, useState } from "react";
+import {useLocation, useNavigate, Link, useParams} from "react-router-dom";
+import ReactMarkdown from "react-markdown";
+import { Note } from "@sprout/shared-types";
+import apiClient from "../../lib/apiClient";
+import MainLayout from "../../components/layout/MainLayout";
+import Tag from "../../components/ui/Tag";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import "dayjs/locale/ko";
+import LineSkeleton from "../../components/ui/LineSkeleton";
+import TopBar from "@/components/ui/TopBar";
+import {Star, Trash2, Copy} from "lucide-react";
+import clsx from "clsx";
+
+dayjs.extend(relativeTime);
+dayjs.locale("ko");
+
+const NotePage: React.FC = () => {
+    const location = useLocation();
+    const [note, setNote] = useState<Note | null>(null);
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+    const [updatedAt, setUpdatedAt] = useState('');
+    const [tags, setTags] = useState<string[]>([]);
+    const [folder, setFolder] = useState<string | null>('');
+    const [isFavorite, setIsFavorite] = useState(false);
+
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null)
+    const navigate = useNavigate();
+
+    const { id } = useParams<{ id: string }>();
+
+    useEffect(() => {
+        const fetchNote = async () => {
+            try {
+                const response = await apiClient.get(`/api/notes/${id}`);
+                setNote(response.data.data);
+            } catch (err) {
+                setError("노트를 불러오는 데 실패했습니다.");
+                console.error(err);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        if (location.state) {
+            setNote(location.state.data);
+        } else {
+            fetchNote();
+        }
+    }, [id]);
+
+    useEffect(() => {
+        if (note) {
+            setTitle(note.title);
+            setContent(note.content || '');
+            setUpdatedAt(note.updatedAt);
+            setTags(note.tags);
+            setFolder(note.folderId);
+            setIsFavorite(note.isFavorite);
+            setLoading(false);
+        }
+    }, [note]);
+
+    const formatUpdatedAt = (date: string) => {
+        const updatedAt = dayjs(date);
+        return updatedAt.format("YYYY-MM-DD HH:mm") + " · " + updatedAt.fromNow();
+    }
+
+    const handleDelete = async () => {
+        if (!note || !id) return;
+
+        if (window.confirm("정말로 이 노트를 삭제하시겠습니까?")) {
+            try {
+                await apiClient.delete(`/api/notes/${id}`);
+                alert("노트가 성공적으로 삭제되었습니다.");
+                navigate('/notes');
+            } catch (err) {
+                alert("노트 삭제에 실패했습니다. 다시 시도해주세요.");
+                console.log("Error delete note: ", err);
+            }
+        }
+    };
+
+    const handleToggleFavorite = async () => {
+        if (!note || !id) return;
+
+        setIsFavorite(!isFavorite);
+        try {
+            await apiClient.post(`/api/notes/${id}/favorite`);
+        } catch (err) {
+            alert("즐겨찾기 상태 변경에 실패했습니다.");
+            setIsFavorite(!isFavorite);
+            console.error(err);
+        }
+    };
+
+    const handleCopy = async () => {
+        if (!note || !id) return;
+
+        try {
+            const response = await apiClient.post(`/api/notes/${id}/copy`);
+            const newNote = response.data.data;
+
+            navigate(`/notes/${newNote.id}`, { state: { data: newNote }});
+        } catch (err) {
+            console.error("Error copy note: ", err);
+            alert("노트 복제에 실패하였습니다. 다시 시도해주세요.");
+        }
+    };
+
+    return (
+      <MainLayout>
+          <TopBar>
+              <span className="text-gray-600">{title}</span>
+              <div className="flex flex-row gap-1 items-center">
+                  <button className={clsx("p-1 hover:bg-gray-100 rounded cursor-pointer", isFavorite ? "text-yellow-500" : "text-gray-500") }
+                        onClick={handleToggleFavorite}>
+                      <Star size={20} strokeWidth={1.5} fill={isFavorite ? 'currentColor' : 'none'} />
+                  </button>
+                  <button className="p-1 text-gray-500 hover:bg-gray-100 rounded cursor-pointer"
+                        onClick={handleCopy}>
+                      <Copy size={20} strokeWidth={1.5} />
+                  </button>
+                  <button className="p-1 text-gray-500 hover:bg-gray-100 rounded cursor-pointer"
+                        onClick={handleDelete}>
+                      <Trash2 size={20} strokeWidth={1.5} />
+                  </button>
+              </div>
+          </TopBar>
+        <div className="text-gray-800 container mx-auto p-8">
+          <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
+            {/* Title Section */}
+            <div className="mb-6">
+                {loading ? (
+                    <LineSkeleton width="w-80" height="h-10" rounded="rounded-lg" />
+                ) : (
+                    <input
+                        type="text"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        placeholder="Untitled"
+                        className="w-full text-4xl font-semibold text-gray-800 border-none focus:outline-none px-0"
+                    />
+                )}
+            </div>
+
+            <hr className="mb-6 text-gray-200" />
+
+            {/* Meta Info Section */}
+              {loading ? (
+                  <div className="flex flex-col gap-4">
+                      <LineSkeleton width="w-60" height="h-5" rounded="rounded-lg" />
+                      <LineSkeleton width="w-60" height="h-5" rounded="rounded-lg" />
+                      <LineSkeleton width="w-60" height="h-5" rounded="rounded-lg" />
+                  </div>
+              ) : (
+                  <div className="flex flex-col gap-4 text-sm">
+                      <div className="flex items-center flex-row gap-2">
+                          <span className="text-gray-500 w-28">Last Updated</span>
+                          <span className="text-gray-700">
+                              {formatUpdatedAt(updatedAt)}
+                          </span>
+                      </div>
+                      <div className="flex items-center flex-row gap-2">
+                          <span className="text-gray-500 w-28">Folder</span>
+                          <div>
+                              <span>Add Folder</span>
+                          </div>
+                      </div>
+                      <div className="flex items-center flex-row gap-2">
+                          <span className="text-gray-500 w-28">Tags</span>
+                          <div>
+                              <span>Add Tags</span>
+                          </div>
+                      </div>
+                  </div>
+              )}
+
+            <hr className="my-6 text-gray-200" />
+
+            {/* Content Section */}
+            <div>
+                {loading ? (
+                    <div className="flex flex-col gap-1">
+                        <LineSkeleton height="h-6" rounded="rounded-lg" />
+                        <LineSkeleton height="h-6" rounded="rounded-lg" />
+                    </div>
+                ) : (
+                    <textarea
+                        value={content}
+                        onChange={(e) => setContent(e.target.value)}
+                        placeholder="오늘 어떤 생각을 하셨나요?"
+                        className="w-full h-screen-minus-86 text-base text-gray-800 border-none focus:outline-none px-0 resize-none"
+                    />
+                )}
+            </div>
+          </div>
+        </div>
+      </MainLayout>
+    );
+};
+
+export default NotePage;

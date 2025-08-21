05a1ec90bae5fdf42a952b1cd472f4ee7f023e6d
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-08-21 20:18:21 +0900

    feat: implement createTag and deleteTag functionalities
    - Controller, Service 단에 createTag 및 deleteTag 메서드 추가

--- apps/backend/src/main/kotlin/chloe/sprout/backend/controller/TagController.kt
+++ apps/backend/src/main/kotlin/chloe/sprout/backend/controller/TagController.kt
@@ -1,22 +1,42 @@
 package chloe.sprout.backend.controller
 
 import chloe.sprout.backend.auth.CustomUserDetails
+import chloe.sprout.backend.dto.TagCreateRequest
+import chloe.sprout.backend.dto.TagCreateResponse
 import chloe.sprout.backend.dto.TagListResponse
 import chloe.sprout.backend.service.TagService
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 
 @RestController
 @RequestMapping("/api/tags")
 class TagController(
     private val tagService: TagService
 ) {
+    @PostMapping
+    fun createTag(@AuthenticationPrincipal user: CustomUserDetails, @RequestBody request: TagCreateRequest): ResponseEntity<TagCreateResponse> {
+        val response = tagService.createTag(user.getUserId(), request)
+        return ResponseEntity.status(HttpStatus.CREATED).body(response)
+    }
+
     @GetMapping
     fun getTagsByOwnerId(@AuthenticationPrincipal user: CustomUserDetails): ResponseEntity<List<TagListResponse>> {
         val response = tagService.getAllTagsByOwnerId(user.getUserId())
         return ResponseEntity.ok(response)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteTag(@AuthenticationPrincipal user: CustomUserDetails, @PathVariable id: UUID): ResponseEntity<Void> {
+        tagService.deleteTag(user.getUserId(), id)
+        return ResponseEntity.noContent().build()
     }
 }
--- apps/backend/src/main/kotlin/chloe/sprout/backend/service/TagService.kt
+++ apps/backend/src/main/kotlin/chloe/sprout/backend/service/TagService.kt
@@ -1,18 +1,75 @@
 package chloe.sprout.backend.service
 
+import chloe.sprout.backend.domain.Tag
+import chloe.sprout.backend.dto.NoteCreateResponse
+import chloe.sprout.backend.dto.TagCreateRequest
+import chloe.sprout.backend.dto.TagCreateResponse
+import chloe.sprout.backend.dto.TagDetailResponse
 import chloe.sprout.backend.dto.TagListResponse
+import chloe.sprout.backend.exception.tag.TagNameAlreadyExistsException
+import chloe.sprout.backend.exception.tag.TagNameRequiredException
+import chloe.sprout.backend.exception.tag.TagNotFoundException
+import chloe.sprout.backend.exception.tag.TagOwnerMismatchException
+import chloe.sprout.backend.exception.user.UserNotFoundException
 import chloe.sprout.backend.repository.TagRepository
+import chloe.sprout.backend.repository.UserRepository
 import jakarta.transaction.Transactional
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import java.util.UUID
 
 @Service
 class TagService(
-    private val tagRepository: TagRepository
+    private val tagRepository: TagRepository,
+    private val userRepository: UserRepository
 ) {
+    @Transactional
+    fun createTag(userId: UUID, request: TagCreateRequest): TagCreateResponse {
+        // User 확인
+        val user = userRepository.findByIdOrNull(userId)
+            ?: throw UserNotFoundException()
+
+        // name blank 여부 확인
+        if (request.name.isBlank()) {
+            throw TagNameRequiredException()
+        }
+
+        // Tag name 중복 검사
+        tagRepository.findByNameAndOwner(request.name, user)?.let {
+            throw TagNameAlreadyExistsException()
+        }
+
+        // Tag entity 생성
+        val tag = Tag(
+            name = request.name,
+            owner = user
+        )
+
+        // DB 저장
+        val save = tagRepository.save(tag)
+
+        // response DTO로 변환 후 반환
+        return TagCreateResponse.from(save)
+    }
+
     @Transactional
     fun getAllTagsByOwnerId(userId: UUID): List<TagListResponse> {
         // Tag 리스트를 response DTO 형태로 변환 후 응답
         return tagRepository.findAllByOwnerId(userId).map { TagListResponse.from(it) }
+    }
+
+    @Transactional
+    fun deleteTag(userId: UUID, tagId: UUID) {
+        // Tag 확인
+        val tag = tagRepository.findByIdOrNull(tagId)
+            ?: throw TagNotFoundException()
+
+        // owner 일치 여부 확인
+        if (tag.owner.id != userId) {
+            throw TagOwnerMismatchException()
+        }
+
+        // Tag 삭제
+        tagRepository.delete(tag)
     }
 }

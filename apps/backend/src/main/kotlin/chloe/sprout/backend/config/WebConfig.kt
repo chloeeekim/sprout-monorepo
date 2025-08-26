e55ab23b8070c15da5ef44b97d2dd327ffdc5831
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-08-26 22:18:05 +0900

    chore: add PATCH to allowedMethods in WebConfig
    - PATCH 메서드 허용

--- apps/backend/src/main/kotlin/chloe/sprout/backend/config/WebConfig.kt
+++ apps/backend/src/main/kotlin/chloe/sprout/backend/config/WebConfig.kt
@@ -10,7 +10,7 @@ class WebConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
             .allowedOrigins("http://localhost:5173") // frontend URL 허용
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
             .allowedHeaders("*") // 모든 헤더 허용
             .allowCredentials(true) // 자격 증명 허용
             .maxAge(3600) // pre-flight 요청 캐싱 시간 (초)

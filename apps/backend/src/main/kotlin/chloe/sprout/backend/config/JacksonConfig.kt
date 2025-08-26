b08b32024daf77dfa696a800e87b524923315dc2
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-08-26 22:18:52 +0900

    chore: add JsonNullable configuration in JacksonConfig
    - PATCH 메서드 구현을 위해 JsonNullable 관련 설정 추가

--- apps/backend/src/main/kotlin/chloe/sprout/backend/config/JacksonConfig.kt
+++ apps/backend/src/main/kotlin/chloe/sprout/backend/config/JacksonConfig.kt
@@ -1,9 +1,12 @@
 package chloe.sprout.backend.config
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.databind.*
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.kotlinModule
+import org.openapitools.jackson.nullable.JsonNullableModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.time.LocalDateTime
@@ -28,10 +31,17 @@ class JacksonConfig {
                 }
             )
 
-        return ObjectMapper()
-            .registerModule(kotlinModule())
-            .registerModule(javaTimeModule)
-            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-            .disable(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE)
+        return jacksonObjectMapper().apply {
+            registerModule(kotlinModule())
+            registerModule(javaTimeModule)
+            disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            disable(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE)
+
+            // JsonNullableModule 관련
+            setSerializationInclusion(JsonInclude.Include.ALWAYS)
+            configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            disable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
+            registerModule(JsonNullableModule())
+        }
     }
 }

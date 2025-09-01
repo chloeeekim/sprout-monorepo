a65171e5a8f203415ad40d4a77fd1e49860bf401
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-09-01 17:13:50 +0900

    refactor: make Note type generic to allow flexible tags typing
    - Note<TTags> 형태로 정의
    - tags를 상황에 따라 string[], TagDetailResponse[] 등으로 활용 가능

--- packages/shared-types/note.ts
+++ packages/shared-types/note.ts
@@ -1,22 +1,22 @@
 import {TagDetailResponse} from "./tag";
 
-export interface Note {
+export interface Note<TTags> {
     id: string;
     title: string;
     content: string | null;
     isFavorite: boolean;
-    tags: TagDetailResponse[];
+    tags: TTags;
     createdAt: string; // ISO 8601 format string
     updatedAt: string; // ISO 8601 format string
     folderId: string | null;
 }
 
 // request types
-export type NoteCreateRequest = Pick<Note, 'title' | 'content' | 'tags'> & { folderId?: string };
-export type NoteUpdateRequest = Pick<Note, 'title' | 'content' | 'tags'> & { folderId?: string };
+export type NoteCreateRequest = Pick<Note<string[]>, "title" | "content" | "tags" | "folderId">;
+export type NoteUpdateRequest = Partial<Pick<Note<string[]>, "title" | "content" | "tags" | "folderId">>;
 
 // response types
-export type NoteCreateResponse = Note;
-export type NoteUpdateResponse = Note;
-export type NoteDetailResponse = Note;
-export type NoteListResponse = Note;
+export type NoteCreateResponse = Note<TagDetailResponse[]>;
+export type NoteUpdateResponse = Note<TagDetailResponse[]>;
+export type NoteDetailResponse = Note<TagDetailResponse[]>;
+export type NoteListResponse = Note<TagDetailResponse[]>;

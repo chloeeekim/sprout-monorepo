11e5b39eba6976922bada2f8293f23aa70c258ed
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-09-10 23:28:45 +0900

    fix: fix TS2307 error
    - 경로 alias 문제 해결을 위해 설정 파일 수정

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -2,12 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-      react(),
-      tsconfigPaths(),
-      tailwindcss()
-  ],
+    plugins: [
+        react(),
+        tsconfigPaths({
+            projects: ['./tsconfig.json']
+        }),
+        tailwindcss()
+    ],
+    resolve: {
+      alias: {
+          '@': path.resolve(__dirname, "./src")
+      }
+    }
 })

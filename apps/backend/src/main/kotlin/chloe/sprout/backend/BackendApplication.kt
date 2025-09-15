014f9d42ca0c113defcb14c58100a278e0f85495
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-09-16 01:25:45 +0900

    chore: remove temporary code for checking AWS credentials

--- apps/backend/src/main/kotlin/chloe/sprout/backend/BackendApplication.kt
+++ apps/backend/src/main/kotlin/chloe/sprout/backend/BackendApplication.kt
@@ -2,13 +2,9 @@ package chloe.sprout.backend
 
 import chloe.sprout.backend.property.OpenAiProperties
 import io.awspring.cloud.autoconfigure.sqs.SqsAutoConfiguration
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
-import org.springframework.stereotype.Component
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider
 
 @SpringBootApplication(exclude = [SqsAutoConfiguration::class])
 @EnableConfigurationProperties(OpenAiProperties::class)
@@ -16,16 +12,4 @@ class BackendApplication
 
 fun main(args: Array<String>) {
 	runApplication<BackendApplication>(*args)
-}
-
-@Component
-class Runner : CommandLineRunner {
-    override fun run(vararg args: String?) {
-        println("=== Check Credentials Start ===")
-
-        val creds = DefaultCredentialsProvider.create().resolveCredentials()
-        println("=== AccessKeyId: ${creds.accessKeyId().toString()}")
-
-        println("=== Check Credentials End ===")
-    }
 }

a36608acb614e317d8d863b89c9b1dd6d7e22a65
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-09-19 15:31:29 +0900

    fix: change return type of getAllNoteLinks to array

--- apps/frontend/src/lib/noteLinkApi.tsx
+++ apps/frontend/src/lib/noteLinkApi.tsx
@@ -1,7 +1,7 @@
 import apiClient from "./apiClient";
 import type { NoteLinkResponse } from '@sprout/shared-types/noteLink';
 
-export const getAllNoteLinks = async (): Promise<NoteLinkResponse> => {
+export const getAllNoteLinks = async (): Promise<NoteLinkResponse[]> => {
     const response = await apiClient.get(`/api/links`);
     return response.data.data;
 }

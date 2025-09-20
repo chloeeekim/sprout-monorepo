e4ecd70480f331da85e24014e7e89a741ae9b037
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-09-20 17:41:04 +0900

    fix: add Vercel app URL to WebConfig allowedOrigins to fix CORS issues

--- apps/backend/src/main/kotlin/chloe/sprout/backend/config/WebConfig.kt
+++ apps/backend/src/main/kotlin/chloe/sprout/backend/config/WebConfig.kt
@@ -9,7 +9,7 @@ class WebConfig : WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
-            .allowedOrigins("http://localhost:5173") // frontend URL 허용
+            .allowedOrigins("http://localhost:5173", "http://sproutnote.vercel.app") // frontend URL 허용
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
             .allowedHeaders("*") // 모든 헤더 허용
             .allowCredentials(true) // 자격 증명 허용
